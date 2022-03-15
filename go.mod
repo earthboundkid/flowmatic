c535c30fddbdb6ed164a43871bc4b4c9b7c3f8e9
Carl Johnson <me@carlmjohnson.net>
2022-03-15 11:04:42 -0400

    Process: Better example

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.18
 
 require github.com/carlmjohnson/deque v0.0.0-20220314201701-25d6e8c963fb
 
-require golang.org/x/exp v0.0.0-20220209042442-160e291fcf24 // indirect
+require golang.org/x/exp v0.0.0-20220314205449-43aec2f8a4e7
--- process_test.go
+++ process_test.go
@@ -2,41 +2,93 @@ package workgroup_test
 
 import (
 	"fmt"
-	"math"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing/fstest"
 
 	"github.com/carlmjohnson/workgroup"
+	"golang.org/x/exp/maps"
+	"golang.org/x/exp/slices"
 )
 
 func ExampleProcess() {
-	var results []struct {
-		int
-		float64
+	// Example site to crawl with recursive links
+	srv := httptest.NewServer(http.FileServer(http.FS(fstest.MapFS{
+		"index.html": &fstest.MapFile{
+			Data: []byte("/a.html"),
+		},
+		"a.html": &fstest.MapFile{
+			Data: []byte("/b1.html\n/b2.html"),
+		},
+		"b1.html": &fstest.MapFile{
+			Data: []byte("/c.html"),
+		},
+		"b2.html": &fstest.MapFile{
+			Data: []byte("/c.html"),
+		},
+		"c.html": &fstest.MapFile{
+			Data: []byte("/"),
+		},
+	})))
+	defer srv.Close()
+	cl := srv.Client()
+
+	// Manager keeps track of which pages have been visited and results graph
+	seen := map[string]bool{}
+	results := map[string][]string{}
+
+	err := workgroup.Process(-1, func(u string) ([]string, error) {
+		res, err := cl.Get(srv.URL + u)
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		return strings.Split(string(body), "\n"), nil
+	}, func(req string, urls []string, err error) ([]string, error) {
+		if err != nil {
+			return nil, err
+		}
+		results[req] = urls
+		var newurls []string
+		for _, u := range urls {
+			if !seen[u] {
+				newurls = append(newurls, u)
+				seen[u] = true
+			}
+		}
+		return newurls, nil
+	}, "/")
+
+	if err != nil {
+		fmt.Println("error", err)
 	}
-	workgroup.Process(1, func(i int) (float64, error) {
-		if i < 0 {
-			return 0, fmt.Errorf("imaginary: sqrt(%d)", i)
+
+	keys := maps.Keys(results)
+	slices.Sort(keys)
+	for _, key := range keys {
+		fmt.Println(key, "links to:")
+		for _, v := range results[key] {
+			fmt.Println("- ", v)
 		}
-		return math.Sqrt(float64(i)), nil
-	}, func(i int, f float64, err error) ([]int, error) {
-		results = append(results, struct {
-			int
-			float64
-		}{i, f})
-		if err == nil && math.Round(f) == f {
-			return []int{int(f)}, nil
-		}
-		return nil, nil
-	}, 256, 81)
-	for _, r := range results {
-		fmt.Printf("sqrt(%d) ~= %.2f\n", r.int, r.float64)
 	}
 
 	// Output:
-	// sqrt(256) ~= 16.00
-	// sqrt(81) ~= 9.00
-	// sqrt(16) ~= 4.00
-	// sqrt(9) ~= 3.00
-	// sqrt(4) ~= 2.00
-	// sqrt(3) ~= 1.73
-	// sqrt(2) ~= 1.41
+	// / links to:
+	// -  /a.html
+	// /a.html links to:
+	// -  /b1.html
+	// -  /b2.html
+	// /b1.html links to:
+	// -  /c.html
+	// /b2.html links to:
+	// -  /c.html
+	// /c.html links to:
+	// -  /
 }

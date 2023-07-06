49bc9db67ee8ea6f053a2ad5e78082c3a7e0fc73
Carl Johnson <me@carlmjohnson.net>
2023-07-06 10:54:23 -0400

    Split up example_test.go

---
+++ do_example_test.go
@@ -0,0 +1,35 @@
+package flowmatic_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/carlmjohnson/flowmatic"
+)
+
+func ExampleDo() {
+	start := time.Now()
+	err := flowmatic.Do(
+		func() error {
+			time.Sleep(50 * time.Millisecond)
+			fmt.Println("hello")
+			return nil
+		}, func() error {
+			time.Sleep(100 * time.Millisecond)
+			fmt.Println("world")
+			return nil
+		}, func() error {
+			time.Sleep(200 * time.Millisecond)
+			fmt.Println("from flowmatic.Do")
+			return nil
+		})
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	fmt.Println("executed concurrently?", time.Since(start) < 250*time.Millisecond)
+	// Output:
+	// hello
+	// world
+	// from flowmatic.Do
+	// executed concurrently? true
+}

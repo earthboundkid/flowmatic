acda10b5d09681ca6a6cddad0a1c7a5b20fc1c88
Carl Johnson <me@carlmjohnson.net>
2023-07-11 10:26:33 -0400

    Tests: Extract common sleepFor

--- all_example_test.go
+++ all_example_test.go
@@ -9,26 +9,22 @@ import (
 )
 
 func ExampleDoContext() {
-	sleepFor := func(d time.Duration) func(context.Context) error {
+	task := func(d time.Duration) func(context.Context) error {
 		return func(ctx context.Context) error {
-			timer := time.NewTimer(d)
-			defer timer.Stop()
-			select {
-			case <-timer.C:
+			if sleepFor(ctx, d) {
 				fmt.Println("timer:", d)
 				return fmt.Errorf("abort!")
-			case <-ctx.Done():
-				fmt.Println("cancelled")
-				return nil
 			}
+			fmt.Println("cancelled")
+			return nil
 		}
 	}
 	ctx := context.Background()
 	start := time.Now()
 	err := flowmatic.DoContext(ctx,
-		sleepFor(1*time.Millisecond),
-		sleepFor(10*time.Millisecond),
-		sleepFor(100*time.Millisecond),
+		task(1*time.Millisecond),
+		task(10*time.Millisecond),
+		task(100*time.Millisecond),
 	)
 	fmt.Println("err:", err)
 	fmt.Println("duration:", time.Since(start).Round(time.Second))

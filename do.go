8c61ef5a425bd731e37cd1bdc2f01750d4e18f5d
Carl Johnson <me@carlmjohnson.net>
2023-07-24 15:10:46 -0400

    refactor: move code from Each to EachN

--- do.go
+++ do.go
@@ -1,12 +1,58 @@
 package flowmatic
 
+import (
+	"errors"
+	"sync"
+)
+
 // Do starts each function in its own goroutine.
 // Errors returned by a function do not halt execution,
 // but are joined into a multierror return value.
 // If a function panics during execution,
 // a panic will be caught and rethrown in the parent Goroutine.
 func Do(fns ...func() error) error {
-	return Each(len(fns), fns, func(in func() error) error {
-		return in()
-	})
+	type result struct {
+		err   error
+		panic any
+	}
+
+	var wg sync.WaitGroup
+	errch := make(chan result, len(fns))
+
+	wg.Add(len(fns))
+	for i := range fns {
+		fn := fns[i]
+		go func() {
+			defer wg.Done()
+			defer func() {
+				if panicVal := recover(); panicVal != nil {
+					errch <- result{panic: panicVal}
+				}
+			}()
+			errch <- result{err: fn()}
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(errch)
+	}()
+
+	var (
+		panicVal any
+		errs     []error
+	)
+	for res := range errch {
+		switch {
+		case res.err == nil && res.panic == nil:
+			continue
+		case res.panic != nil:
+			panicVal = res.panic
+		case res.err != nil:
+			errs = append(errs, res.err)
+		}
+	}
+	if panicVal != nil {
+		panic(panicVal)
+	}
+	return errors.Join(errs...)
 }
--- each.go
+++ each.go
@@ -9,30 +9,9 @@ import "errors"
 // If a task panics during execution,
 // the panic will be caught and rethrown in the parent Goroutine.
 func Each[Input any](numWorkers int, items []Input, task func(Input) error) error {
-	var recovered any
-	errs := make([]error, 0, len(items))
-	runner := func(in Input) (r any, err error) {
-		defer func() {
-			r = recover()
-		}()
-		err = task(in)
-		return
-	}
-	manager := func(_ Input, r any, err error) ([]Input, bool) {
-		if r != nil {
-			recovered = r
-		}
-		if err != nil {
-			errs = append(errs, err)
-		}
-		return nil, true
-	}
-	ManageTasks(numWorkers, runner, manager, items...)
-
-	if recovered != nil {
-		panic(recovered)
-	}
-	return errors.Join(errs...)
+	return EachN(numWorkers, len(items), func(pos int) error {
+		return task(items[pos])
+	})
 }
 
 // EachN starts numWorkers concurrent workers (or GOMAXPROCS workers if numWorkers < 1)

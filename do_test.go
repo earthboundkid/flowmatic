a164a961b9089f7126ff17618364c1d65cb45a67
Carl Johnson <me@carlmjohnson.net>
2023-07-24 15:44:50 -0400

    tests: Increase coverage

---
+++ do_test.go
@@ -0,0 +1,23 @@
+package flowmatic_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/carlmjohnson/flowmatic"
+)
+
+func TestDo_err(t *testing.T) {
+	a := errors.New("a")
+	b := errors.New("b")
+	errs := flowmatic.Do(
+		func() error { return a },
+		func() error { return b },
+	)
+	if !errors.Is(errs, a) {
+		t.Fatal(errs)
+	}
+	if !errors.Is(errs, b) {
+		t.Fatal(errs)
+	}
+}

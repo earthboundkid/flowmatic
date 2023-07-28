8bfa02b377b38310f0593fdec63ee6baa4cf2229
Carl Johnson <me@carlmjohnson.net>
2023-07-28 14:53:45 -0400

    Tests: Increase test coverage

---
+++ map_test.go
@@ -0,0 +1,34 @@
+package flowmatic_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/carlmjohnson/flowmatic"
+)
+
+func TestMap(t *testing.T) {
+	ctx := context.Background()
+	a := errors.New("a")
+	b := errors.New("b")
+	o, errs := flowmatic.Map(ctx, 1, []int{1, 2, 3}, func(_ context.Context, i int) (int, error) {
+		switch i {
+		case 1:
+			return 1, a
+		case 2:
+			return 2, b
+		default:
+			panic("should be canceled by now!")
+		}
+	})
+	if !errors.Is(errs, a) {
+		t.Fatal(errs)
+	}
+	if errors.Is(errs, b) {
+		t.Fatal(errs)
+	}
+	if o != nil {
+		t.Fatal(o)
+	}
+}
--- panic_test.go
+++ panic_test.go
@@ -170,3 +170,32 @@ func TestDoContext_panic(t *testing.T) {
 		t.Fatal(n.Load())
 	}
 }
+
+func TestMap_panic(t *testing.T) {
+	var (
+		err error
+		o   []int64
+	)
+	ctx := context.Background()
+	r := try(func() {
+		o, err = flowmatic.Map(ctx, 1, []int64{1, 2, 3},
+			func(_ context.Context, delta int64) (int64, error) {
+				if delta == 2 {
+					panic("boom")
+				}
+				return 2 * delta, nil
+			})
+	})
+	if err != nil {
+		t.Fatal("should have panicked")
+	}
+	if r == nil {
+		t.Fatal("should have panicked")
+	}
+	if r != "boom" {
+		t.Fatal(r)
+	}
+	if o != nil {
+		t.Fatal(o)
+	}
+}

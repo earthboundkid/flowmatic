5a86cbd493d143a27f90a7cefa54a988b16dece5
Carl Johnson <me@carlmjohnson.net>
2023-07-30 20:16:28 -0400

    Docs: Update All, Do, Race

--- all.go
+++ all.go
@@ -4,20 +4,20 @@ import (
 	"context"
 )
 
-// All runs fns in concurrently
+// All runs each task concurrently
 // and waits for them all to finish.
-// Each function receives a child context
-// which is cancelled once one function returns an error or panics.
-// All returns nil if all functions succeed.
+// Each task receives a child context
+// which is cancelled once one task returns an error or panics.
+// All returns nil if all tasks succeed.
 // Otherwise,
 // All returns a multierror containing the errors encountered.
-// If a function panics during execution,
+// If a task panics during execution,
 // a panic will be caught and rethrown in the parent Goroutine.
-func All(ctx context.Context, fns ...func(context.Context) error) error {
+func All(ctx context.Context, tasks ...func(context.Context) error) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	return eachN(len(fns), len(fns), func(pos int) error {
+	return eachN(len(tasks), len(tasks), func(pos int) error {
 		defer func() {
 			panicVal := recover()
 			if panicVal != nil {
@@ -25,7 +25,7 @@ func All(ctx context.Context, fns ...func(context.Context) error) error {
 				panic(panicVal)
 			}
 		}()
-		err := fns[pos](ctx)
+		err := tasks[pos](ctx)
 		if err != nil {
 			cancel()
 			return err

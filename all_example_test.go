981b0c8e348dc83bc7fee8ec112c4f09aebe6fb6
Carl Johnson <me@carlmjohnson.net>
2023-08-04 13:55:43 -0400

    ExampleAll: Clearer example

--- all_example_test.go
+++ all_example_test.go
@@ -9,30 +9,31 @@ import (
 )
 
 func ExampleAll() {
-	task := func(d time.Duration) func(context.Context) error {
-		return func(ctx context.Context) error {
-			// sleepFor is a cancellable time.Sleep
-			if !sleepFor(ctx, d) {
-				fmt.Println("cancelled")
-				return nil
-			}
-			fmt.Println("slept for", d)
-			return fmt.Errorf("abort after %v", d)
-		}
-	}
 	ctx := context.Background()
 	start := time.Now()
 	err := flowmatic.All(ctx,
-		task(1*time.Millisecond),
-		task(10*time.Millisecond),
-		task(100*time.Millisecond),
+		func(ctx context.Context) error {
+			// This task sleeps then returns an error
+			d := 1 * time.Millisecond
+			time.Sleep(d)
+			fmt.Println("slept for", d)
+			return fmt.Errorf("abort after %v", d)
+		},
+		func(ctx context.Context) error {
+			// sleepFor is a cancelable time.Sleep.
+			// The error of first task
+			// causes the early cancelation of this one.
+			if !sleepFor(ctx, 1*time.Minute) {
+				fmt.Println("canceled")
+			}
+			return nil
+		},
 	)
 	fmt.Println("err:", err)
 	fmt.Println("exited early?", time.Since(start) < 10*time.Millisecond)
 	// Output:
 	// slept for 1ms
-	// cancelled
-	// cancelled
+	// canceled
 	// err: abort after 1ms
 	// exited early? true
 }

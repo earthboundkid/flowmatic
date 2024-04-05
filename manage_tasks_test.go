ec179b61f0583c54dbb97565893befb24b9b1ca7
Carlana Johnson <mail@carlana.net>
2024-04-05 18:18:40 -0400

    Test that it's sleeping enough

--- manage_tasks_test.go
+++ manage_tasks_test.go
@@ -44,3 +44,39 @@ func TestManageTasks_drainage(t *testing.T) {
 		t.Fatal("didn't finish")
 	}
 }
+
+func TestManageTasks_drainage2(t *testing.T) {
+	const shortSleepTime = 10 * time.Millisecond
+	const longSleepTime = 30 * time.Millisecond
+	b := false
+	task := func(n int) (int, error) {
+		if n == 1 {
+			time.Sleep(longSleepTime)
+			b = true
+			return 1, errors.New("text string")
+		}
+		return 0, errors.New("-")
+	}
+	start := time.Now()
+	m := map[int]struct {
+		int
+		error
+	}{}
+	for r := range flowmatic.Tasks(2, task, 0, 1) {
+		m[r.In] = struct {
+			int
+			error
+		}{r.Out, r.Err}
+		time.Sleep(shortSleepTime)
+		break
+	}
+	if s := fmt.Sprint(m); s != "map[0:-]" {
+		t.Fatal(s)
+	}
+	if time.Since(start) < longSleepTime {
+		t.Fatal("didn't sleep enough")
+	}
+	if !b {
+		t.Fatal("didn't finish")
+	}
+}
